07bc8db3b58dc19ef09cb30b1ea3195430370685
Lucas Burroni <desarrollowebburroni@gmail.com>
2021-10-05 02:49:07 -0300

    ejs, algo de css

--- public/index.js
+++ public/index.js
@@ -1,29 +1,29 @@
 const d = document;
 const socket = io();
-
 const ul = d.getElementById("ul");
-// datos form
-const tittle = d.getElementById('tittle').value
-const price = d.getElementById('price').value
-const thumbail = d.getElementById('thumbail').value
 
 socket.on("PRODUCTOS", (data) => {
-  toRender(data)
+  toRender(data);
 });
 
 let toRender = (data) => {
-  let pantilla = data.map((e) => `
-<li class='grid'>  
-<em> ${e.tittle}</em>
-<p>${e.price}</p>
-<img class='imagen' src='${e.thumbail}' alt="">
-</li>`
-    ).join(" ");
-    ul.innerHTML = pantilla;  
-};  
+  let pantilla = data
+    .map(
+      (e) => `
+  <li class='grid'>  
+  <em> ${e.tittle}</em>
+  <p>${e.price}</p>
+  <img class='imagen' src='${e.thumbail}' alt="">
+  </li>`
+    )
+    .join(" ");
+  ul.innerHTML = pantilla;
+};
 
-const productSend = () =>{
-  // let newProduct = {tittle,price,thumbail}
-  socket.emit("productIn",'probando msj')
-}
-
+const productSend = () => {
+  const tittle = d.getElementById("tittle").value;
+  const price = d.getElementById("price").value;
+  const thumbail = d.getElementById("thumbail").value;
+  let newProduct = { tittle, price, thumbail };
+  socket.emit("productIn", newProduct);
+};
